271164945be295d9f04e7004bcfd3ebfb7329e15
Great Okonkwo <greatokonkwo@outlook.com>
2026-02-02 20:08:07 -0500

    chore: configure Posthog and Sentry

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
+import { withSentryConfig } from "@sentry/nextjs";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
 };
 
-export default nextConfig;
+export default withSentryConfig(nextConfig, {
+  org: "signa-labs-inc",
+  project: "javascript-nextjs",
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
+});
